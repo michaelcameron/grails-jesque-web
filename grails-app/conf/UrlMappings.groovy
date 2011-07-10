604a1d427d0e4f1206a41373d785f3312b09224b
Ted Naleid <contact@naleid.com>
2011-07-10 01:33:48 -0500

    removed println
    --HG--
    branch : alternate-controller-layout

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,7 +1,6 @@
 class UrlMappings {
 
 	static mappings = {
-        println "in mappings"
         "/jesque/queues/$queueName" (controller:"jesque", action:"queueDetail")
         "/jesque/stats/$statType" (controller:"jesque", action:"stats")
 
